744a5c823beef3398123e2bcd1b80cb1d1b9b77b
mshhour <mmm770255652@gmail.com>
2022-06-28 10:36:22 +0300

    my second file

---
+++ map.py
@@ -0,0 +1,14 @@
+# names =["mohmmed ","ali ", "salem"]
+# apper = list( map (str.upper ,names))
+# print(apper)
+
+
+
+
+
+def mshhour (x):
+    return x**9
+numbers = [1,2,3,4]
+num= list(map(mshhour,numbers))
+print(num)
+
